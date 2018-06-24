a9af1f34d0572b6cad58185bcb428fba1bff11e4
MD HALIM <halim.infinity@gmail.com>
2018-06-25 01:08:23 +0530

    pushing front-end code

---
+++ js/custom-jquey.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    function() {
+        $(".joiToggle").click(function() {
+        $(".togglebox").toggle();
+
+    });
+
+    $(function () {
+        $('#datetimepicker1').datetimepicker();
+    });
+});
